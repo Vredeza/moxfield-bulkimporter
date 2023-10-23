dd84a72826b78c5d47ed26e129c24b278670197a
Vredeza <53372645+Vredeza@users.noreply.github.com>
2023-10-24 00:32:25 +0200

    Starting project

---
+++ src/SetInput.js
@@ -0,0 +1,27 @@
+import {useState} from "react";
+import './SetInput.css';
+
+function SetInput({text = 'Import', onImport, placeHolder = ''}) {
+
+    const [set, setSet] = useState("");
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            importData(set);
+        }
+    };
+
+    const importData = () => {
+        if (set.length >= 1) {
+            onImport(set);
+        }
+    }
+
+    return(
+        <div className={"extensionInput"}>
+            <input type={"text"} onKeyDown={handleKeyPress} onChange={(event) => setSet(event.target.value)} placeholder={placeHolder}/>
+            <input type={"button"} value={text} onClick={() => importData(set)} className={set.length >= 1 ? "available" : ""}/>
+        </div>
+    );
+}
+
+export default SetInput;
