eee240f3e5fbd1e33dae157aa81a2d1c852bb611
sanchezj <53372645+Vredeza@users.noreply.github.com>
2023-10-26 12:40:20 +0000

    Using axios package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import './App.css';
 import SetInput from "./SetInput";
+import axios from "axios";
+
+function getCards(searchUri, pageNumber){
+    axios.get(`${searchUri}&page=${pageNumber}`)
+        .then(response=>{
+            if(response.data.has_more){
+                // requête + data
+            }
+            else {
+                // data
+            }
+        })
+    return 'a'
+}
+function getSet(setAcronym){
+    axios.get(`https://api.scryfall.com/sets/${setAcronym}`)
+        .then(response => {
+            console.log("extension trouvée, acquisition des cartes")
+            console.log(getCards(response.data.search_uri, 1));
+        })
+        .catch(error => {
+            console.log("impossible de trouver l'extension")
+        });
+}
 
 function App() {
   return (
@@ -8,6 +32,7 @@ function App() {
       <SetInput
           text={"Select set"}
           placeHolder={"set acronym"}
+          onImport={getSet}
       />
 
     </div>
