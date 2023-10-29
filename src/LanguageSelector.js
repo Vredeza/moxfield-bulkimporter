eac83907665a16d03f20670b38ef881cfbf08086
Vredeza <53372645+Vredeza@users.noreply.github.com>
2023-10-29 10:25:00 +0100

    LanguageSelector is now working properly

--- src/LanguageSelector.js
+++ src/LanguageSelector.js
@@ -2,7 +2,7 @@ import "./LanguageSelector.css"
 
 function LanguageSelector({languageList, setLanguage}) {
     return (
-        <select>
+        <select onChange={(event) => setLanguage(event.target.value)}>
             {languageList.map((language, index) => (
                 <option value={language} key={index}>{language}</option>
             ))}
