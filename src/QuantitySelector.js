eb6c7b2b7896246ae976559a8497778f8f9caadd
sanchezj <53372645+Vredeza@users.noreply.github.com>
2023-11-07 16:23:34 +0100

    Changed the way quantity works : "+" and "-" buttons

--- src/QuantitySelector.js
+++ src/QuantitySelector.js
@@ -1,12 +1,38 @@
 import "./QuantitySelector.css"
+import {useState} from "react";
 
 function QuantitySelector({updateQuantity}) {
 
+    const [quantity, setQuantity] = useState(0)
+
+    const increment = () => {
+        setQuantity(quantity + 1);
+        updateQuantity(quantity);
+    }
+
+    const decrement = () => {
+        if (quantity > 0) {
+            setQuantity(quantity -1);
+            updateQuantity(quantity);
+        }
+    }
+
+    const requestNewValue = () => {
+        let newValue = prompt("New amount : ", quantity.toString())
+        if (!isNaN(newValue)) { // is a number
+            setQuantity(parseFloat(newValue));
+            updateQuantity(parseFloat(newValue));
+        }
+    }
+
     return (
         <div className={"quantitySelector"}>
-            <input type={"number"} onChange={(event) => updateQuantity(event.target.value)} placeholder={"0"}/>
-        </div>
 
+            <button onClick={decrement}>-</button>
+            <button className={quantity === 0 && "null"} onClick={requestNewValue}>{quantity}</button>
+            <button onClick={increment}>+</button>
+
+        </div>
     );
 }
 
